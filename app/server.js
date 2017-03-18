d6a159131d608652e89ada589524aa3a8effb4c2
Tatu Putto <tatu.putto@hotmail.com>
2017-03-19 01:25:11 +0200

    refaktor

--- app/server.js
+++ app/server.js
@@ -5,15 +5,12 @@ const timestamp = require('./timestamp');
 const app = express();
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.get('/', (req, res) => res.render('index'));
 
-app.get('/:time', function(req, res) {
+app.get('/:time', (req, res) => {
     const urlObj = url.parse(req.url, true)
     const parsedPathname = urlObj.pathname.replace(/%20/g, ',');
     const output = timestamp(parsedPathname.substring(1));
-
     res.end(JSON.stringify(output));
 });
 
